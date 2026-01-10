61350bc7a304e8d97bcf834a406d8a2bbb0b70ed
wilfredo-marapacuto <wmarapacutoverastegu@byupathway.edu>
2026-01-09 21:39:09 -0800

    w1.3

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,7 @@
+// Obtener año actual
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
+// Obtener última fecha de modificación del documento
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
