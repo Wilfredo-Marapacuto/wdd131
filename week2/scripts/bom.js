7a71e389a83836e43c699931ad1e866e13a20138
wilfredo-marapacuto <wmarapacutoverastegu@byupathway.edu>
2026-01-17 09:10:00 -0800

    w2.3

---
+++ week2/scripts/bom.js
@@ -0,0 +1,28 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", () => {
+  if (input.value.trim() === "") {
+    input.focus();
+    return;
+  }
+
+  const li = document.createElement("li");
+  const deleteButton = document.createElement("button");
+
+  li.textContent = input.value;
+  deleteButton.textContent = "X";
+  deleteButton.setAttribute("aria-label", "Delete chapter");
+
+  deleteButton.addEventListener("click", () => {
+    list.removeChild(li);
+    input.focus();
+  });
+
+  li.append(deleteButton);
+  list.append(li);
+
+  input.value = "";
+  input.focus();
+});
